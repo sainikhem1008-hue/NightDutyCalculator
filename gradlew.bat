9e3f9dda95d48d729df6198883838ea66f092d5a
sainikhem1008-hue <sainikhem1008@gmail.com>
2025-09-01 19:32:52 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,24 @@
+@echo off
+rem -----------------------------------------------------------------------------
+rem Gradle Startup Script for Windows
+rem -----------------------------------------------------------------------------
+
+setlocal
+
+set SCRIPT_DIR=%~dp0
+set WRAPPER_JAR=%SCRIPT_DIR%gradle\wrapper\gradle-wrapper.jar
+
+if not exist "%WRAPPER_JAR%" (
+    echo Gradle wrapper jar not found: %WRAPPER_JAR%
+    exit /b 1
+)
+
+if "%JAVA_HOME%" == "" (
+    set JAVA_CMD=java
+) else (
+    set JAVA_CMD=%JAVA_HOME%\bin\java
+)
+
+"%JAVA_CMD%" -jar "%WRAPPER_JAR%" %*
+endlocal
 
